7c912f8303074824e80cdef7c9482fa60f13358e
kody rogers <kody@debian.hello.kody>
2019-12-27 14:38:01 -0600

    made descriptions for things in the gallery

--- app/photos/forms.py
+++ app/photos/forms.py
@@ -2,3 +2,21 @@
 # Forms
 #     PhotoForm: Will hold all the fields needed to edit and add new photo entries
 #
+
+#
+# PhotoForm
+# Purpose:
+#     This form will hold all of the fields needed to specify a photo in the database.
+#     It will be used to add and edit a photo
+#
+# Fields:
+#     FileName: StringField (Required)
+#     Category: StringField
+#     Caption: TextAreaField
+#     Submit: SubmitField
+#
+# Things that will need to be included:
+#     StringField, TextAreaField, and SubmitField from wtforms
+#     FlaskForm from flask_wtf
+#     Required from wtforms.validators
+#
--- app/photos/views.py
+++ app/photos/views.py
@@ -1,9 +1,110 @@
+
 #
-# Views
-#     Gallery: The view that will display the photos and accompanying captions
-#     Add Photo: The view that will call the Photo form and add a new photo to the 
-#         the database
-#     Edit Photo: View that will allow the user to change an entry and then update
-#         the database
-#     Delete Photo: Allows the user to delete an entry from the database
+# Gallery
+# Purpose:
+#     It will display all the selected photos
+#
+# Method:
+#     if a category is selected:
+#         search the database for photos in the selected category
+#
+#         search for the photo categories
+#
+#         render the Gallery page with those photos and the gallery categories
+#
+#     else
+#         search the database for all Photos
+#         search the database for photo categories
+#
+#         render the Gallery page with all the photos and the categories
+# Other Functions or classes needed:
+#     render_template from flask
+#     also need database manipulation stuff, and the Post and Get shit
+#
+# Need to look at:
+#     database and Post and Get stuff
+#
+
+#
+# Add Photo
+# Purpose:
+#     Allows the user to enter a new photo in the database
+#
+# Method:
+#     check if user is logged in
+#     create an empty PhotoForm
+#
+#     if a PhotoForm has been validated
+#         create a new Photo instance and make it's values equal to the submited values
+#
+#         try:
+#             adding the new photo
+#             flash a success message
+#         except:
+#             flash that an error has occured
+#
+#         redirect to the Gallery page with that particular photo as an input
+#
+#     render the add photo template
+#
+# Other functions and classes needed:
+#     render_template, flash, and url_for from flask
+#     PhotoForm from forms.py
+#     several database, POST and GET stuff that will be figured later
+#
+
+#
+# Edit Photo
+# Purpose:
+#     To allow the user to edit an existing photo entry in the database
+#
+# Method:
+#     check if user is logged in
+#     Find the specified Photo
+#
+#     Create a PhotoForm filled with the info from the specified Post
+#
+#     if the form has been validated
+#         update the photo with the new information
+#
+#         commit it to the database
+#
+#         flash a success message
+#
+#         redirect to the Gallery page
+#
+#     specify that the form should be filled with the data from the Photo that was collected
+#
+#     render the edit page
+#
+# Other functions and classes needed:
+#     render_template, flash, and url_for from flask
+#     PhotoForm from forms.py
+#     several database, POST and GET stuff that will be figured later
+#
+
+#
+# Delete Photo
+# Purpose:
+#     allow a user to delete a photo from the database
+#
+# Method:
+#     (the link) /gallery/delete/<int: id number> // need GET and POST as methods
+#
+#     check if user is an administrator (logged in)
+#
+#     find the particular photo
+#
+#     delete the photo
+#
+#     commit the changes
+#
+#     flash a success message
+#
+#     redirect to the Gallery
+#
+# Other Functions and such needed
+#     the database (aka db) defined in the init.py
+#     Photo from models
+#     render_template, flash from flask
 #
