97ab364c7ff51c4ad77a77e6b04292f784adacbc
kody rogers <kody@debian.hello.kody>
2019-12-27 10:12:18 -0600

    created an outline for the all the auth functions and forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,15 @@
+
 #
-# will hold the forms for logging and logging out
+# Purpose: This from will be used to collect the information for the user logging
+#     and logging out.
 #
-# Forms:
-#     Login: Will collect the information for logging in a user
+# Fields:
+#     Password: The password to validate the user
+#     Username: This contains the name that a user has chosen to represent them
+#     Submit: This is the field that the user uses to signal that everything has been
+#         filled out.
+#
+# Returns:
+#     All the material that the user filled out (bassically all the fields but filled
+#         out).
 #
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,44 @@
+
+
 #
-# will hold the views for logging in and logging out
+# Login
+# Purpose:
+#     To allow the user login
 #
-# Views:
-#     Login: Will check the credentials and login a user
+# Method:
+#     if form submitted
+#         if Values are correct
+#             login the user
 #
-#     Logout: Logout a user
+#             redirect to home
+#
+#         else
+#             render login and flash that it was incorrect
+#
+#     else
+#         render the login page
+#
+# Other Functions Needed:
+#     login_user from flask_login
+#     redirect, flash, and render_template from flask
+#
+#     All defined so we good :)
+#
+
+#
+# Logout
+# Purpose:
+#     This function will respond when the user clicks the logout link by logging
+#     the user out.
+#
+# Method:
+#     logout user
+#
+#     set a flash to say logout was successful
+#
+#     redirect to the homepage
+#
+# Other Functions Needed:
+#     redirect, flash, url_for from flask
+#     logout_user from flask_login
 #
