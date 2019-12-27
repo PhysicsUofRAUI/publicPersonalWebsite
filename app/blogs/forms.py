c1805b171086341a313e9e7693b7fc462ef09cf5
kody rogers <kody@debian.hello.kody>
2019-12-27 11:58:19 -0600

    made descriptions for the blogs stuff. Need better coverage of the db

--- app/blogs/forms.py
+++ app/blogs/forms.py
@@ -1,5 +1,18 @@
+
 #
-# Forms
-#   PostForm: form to input data into both edit and add. For edit fields will
-#       be filled
+# PostForm
+# Purpose:
+#     Gives the user a way to input information into the post table.
+#
+# Fields:
+#     Title: String Field (Required)
+#     Category: String Field (Required)
+#     Subcategory: String Field
+#     Content: Text Field (Required)
+#     Submit: Submit Field
+#
+# Other Functions or Classes Needed:
+#     TextAreaField, StringField, SubmitField from wtforms
+#     FlaskForm from flask_wtf
+#     Required from wtforms.validators
 #
