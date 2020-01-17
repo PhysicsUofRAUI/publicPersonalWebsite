c9bf13be378164806a641b5634edf8e625bbeaf6
kody rogers <kody@debian.hello.kody>
2020-01-16 18:57:56 -0600

    added code for the run.py

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
+import os
 
-# the driving function
+from app import create_app
+
+config_name = os.getenv('FLASK_CONFIG')
+app = create_app(config_name)
+
+if __name__ == '__main__':
+    app.run()
